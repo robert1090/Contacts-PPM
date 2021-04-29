6cec2e546cec7755bb02a6edc3aad972c097fb8b
robert1090 <avilar516@gmail.com>
2021-04-28 22:59:51 -0400

    Finalizado

--- src/editar_contact.c
+++ src/editar_contact.c
@@ -7,105 +7,110 @@
 void editar_contact(Contacto contactos[]) /* Funcion de Editar contactos */
 {
 
+    int i = 0;
     char temp[50]; /* Variable para buscar nombre */
-    char Opcion; /* Variable para el menu de eliminar */
+    char Opcion;   /* Variable para el menu de eliminar */
 
     printf("\tBienvenido a Editar: \n\n"
            "Ingrese el Nombre del Contacto que desea editar: ");
-    fgets(temp, sizeof(temp), stdin);  /* Esta funcion se encargara de digitar el nombre del contacto a buscar */
+    fgets(temp, sizeof(temp), stdin); /* Esta funcion se encargara de digitar el nombre del contacto a buscar */
     temp[strcspn(temp, "\n")] = 0;
 
-    for (size_t i = 0; i <= 100; i++) /* Busqueta de todos los posibles espacios de contactos */
+    do
     {
-        if (contactos[i].full && contactos[i].eliminado == false)  /* Si existe un contacto y no fue eliminado, este sera comparado*/
+        for (i = 0; i <= 100; i++) /* Busqueta de todos los posibles espacios de contactos */
         {
-            if (strcmp(contactos[i].nombre, temp) == 0)  /* Si un contacto coincide, este sera seleccionado */
+            if (contactos[i].full && contactos[i].eliminado == false) /* Si existe un contacto y no fue eliminado, este sera comparado*/
             {
-                /* Menu de Editar */
-                printf("\nSeleccione la informacion a Editar:\n"
-                       "1) Nombre\n"
-                       "2) Numero\n"
-                       "3) Direccion\n"
-                       "4) Salir\n\n"
-                       "Informacion del Contacto Actual: \n");
-                printf("\nNombre: %s", contactos[i].nombre);  /* Mostrara Nombre del contacto */
-                printf("\nNumero: %s", contactos[i].numero);  /* Mostrara Numero del contacto */
-                printf("\nDireccion: %s\n", contactos[i].direccion);  /* Mostrara Direccion del contacto */
-                scanf("%c", &Opcion);
-                getchar();
-
-                switch (Opcion)
+                if (strcmp(contactos[i].nombre, temp) == 0) /* Si un contacto coincide, este sera seleccionado */
                 {
-                case 1:
-                    
-                    /* Edicion del Nombre */
-                    printf("Nombre: ");
-                    fgets(contactos[i].nombre, sizeof(contactos[i].nombre), stdin);
-                    contactos[i].nombre[strcspn(contactos[i].nombre, "\n")] = 0;
-
-                    printf("Nombre cambiado, volviendo al Menu de Inicio...");
+                    /* Menu de Editar */
+                    printf("\nSeleccione la informacion a Editar:\n"
+                           "1) Nombre\n"
+                           "2) Numero\n"
+                           "3) Direccion\n"
+                           "4) Salir\n\n"
+                           "Informacion del Contacto Actual: \n");
+                    printf("\nNombre: %s", contactos[i].nombre);         /* Mostrara Nombre del contacto */
+                    printf("\nNumero: %s", contactos[i].numero);         /* Mostrara Numero del contacto */
+                    printf("\nDireccion: %s\n", contactos[i].direccion); /* Mostrara Direccion del contacto */
+                    scanf("%c", &Opcion);
                     getchar();
-                    system("cls||clear");
-                    menu();
 
-                    break;
+                    switch (Opcion)
+                    {
+                    case 1:
 
-                case 2:
+                        /* Edicion del Nombre */
+                        printf("Nombre: ");
+                        fgets(contactos[i].nombre, sizeof(contactos[i].nombre), stdin);
+                        contactos[i].nombre[strcspn(contactos[i].nombre, "\n")] = 0;
 
-                    /* Edicion del Numero */
-                    printf("Numero: ");
-                    fgets(contactos[i].numero, sizeof(contactos[i].numero), stdin);
-                    contactos[i].numero[strcspn(contactos[i].numero, "\n")] = 0;
+                        printf("Nombre cambiado, volviendo al Menu de Inicio...");
+                        getchar();
+                        system("cls||clear");
+                        menu();
 
-                    printf("\nNumero cambiado, volviendo al Menu de Inicio...");
-                    getchar();
-                    system("cls||clear");
-                    menu();
+                        break;
 
-                    break;
+                    case 2:
 
-                case 3:
+                        /* Edicion del Numero */
+                        printf("Numero: ");
+                        fgets(contactos[i].numero, sizeof(contactos[i].numero), stdin);
+                        contactos[i].numero[strcspn(contactos[i].numero, "\n")] = 0;
 
-                    /* Edicion de la Direccion */
-                    printf("Direccion: ");
-                    fgets(contactos[i].direccion, sizeof(contactos[i].direccion), stdin);
-                    contactos[i].direccion[strcspn(contactos[i].direccion, "\n")] = 0;
+                        printf("\nNumero cambiado, volviendo al Menu de Inicio...");
+                        getchar();
+                        system("cls||clear");
+                        menu();
 
-                    printf("\nDireccion cambiada, volviendo al Menu de Inicio...");
-                    getchar();
-                    system("cls||clear");
-                    menu();
+                        break;
 
-                    break;
+                    case 3:
 
-                case 4:
+                        /* Edicion de la Direccion */
+                        printf("Direccion: ");
+                        fgets(contactos[i].direccion, sizeof(contactos[i].direccion), stdin);
+                        contactos[i].direccion[strcspn(contactos[i].direccion, "\n")] = 0;
 
-                    /* Devuelta al Menu Principal */
-                    printf("Volviendo al Menu de Inicio...");
-                    getchar();
-                    system("cls||clear");
-                    menu();
+                        printf("\nDireccion cambiada, volviendo al Menu de Inicio...");
+                        getchar();
+                        system("cls||clear");
+                        menu();
 
-                    break;
+                        break;
 
-                default:
+                    case 4:
 
-                    /* Si la opcion no es valida */
-                    printf("Opcion no valida, volviendo al Menu de Inicio...");
-                    getchar();
-                    system("cls||clear");
-                    menu();
-                    break;
+                        /* Devuelta al Menu Principal */
+                        printf("Volviendo al Menu de Inicio...");
+                        getchar();
+                        system("cls||clear");
+                        menu();
+
+                        break;
+
+                    default:
+
+                        /* Si la opcion no es valida */
+                        printf("Opcion no valida, volviendo al Menu de Inicio...");
+                        getchar();
+                        system("cls||clear");
+                        menu();
+                        break;
+                    }
                 }
             }
-            else
-            {
-                /* En caso de no coincidir */
-                printf("No existe un Contacto con ese nombre, volviendo al Menu Principal...");
-                getchar();
-                system("cls||clear");
-                menu();
-            }
         }
+    } while (i < 101);
+
+    if (strcmp(contactos[i].nombre, temp) != 0)
+    {
+        /* Si no coincide ningun contacto existente y que no haya sido eliminado, se devolvera al menu de inicio */
+        printf("No existe un Contacto con ese nombre, volviendo al Menu Principal...");
+        getchar();
+        system("cls||clear");
+        menu();
     }
 }
--- src/eliminar_contact.c
+++ src/eliminar_contact.c
@@ -7,54 +7,60 @@
 void eliminar_contact(Contacto contactos[]) /* Funcion de eliminar contactos */
 {
 
+    int i = 0;
     char temp[50]; /* Variable para buscar nombre */
-    char Opcion; /* Variable para el menu de eliminar */
+    char Opcion;   /* Variable para el menu de eliminar */
 
     printf("\tBienvenido a Eliminar\n"
            "Ingrese el Nombre del Contacto a Eliminar: ");
     fgets(temp, sizeof(temp), stdin); /* Esta funcion se encargara de digitar el nombre del contacto a buscar */
     temp[strcspn(temp, "\n")] = 0;
 
-    for (size_t i = 0; i <= 100; i++) /* Busqueta de todos los posibles espacios de contactos */
+    do
     {
-        if (contactos[i].full && contactos[i].eliminado == false) /* Si existe un contacto y no fue eliminado, este sera comparado*/
+        for (i = 0; i <= 100; i++) /* Busqueta de todos los posibles espacios de contactos */
         {
-            if (strcmp(contactos[i].nombre, temp) == 0) /* Si un contacto coincide, este sera seleccionado */
+            if (contactos[i].full && contactos[i].eliminado == false) /* Si existe un contacto y no fue eliminado, este sera comparado*/
             {
-                /* Menu de Eliminar */
-                printf("\nEsta seguro de que desea eliminar este contacto?(S o N)\n"
-                       "\nInformacion del Contacto:");
-                printf("\nNombre: %s", contactos[i].nombre); /* Mostrara Nombre del contacto */
-                printf("\nNumero: %s", contactos[i].numero); /* Mostrara Numero del contacto */
-                printf("\nDireccion: %s\n", contactos[i].direccion); /* Mostrara Direccion del contacto */
-                scanf("%c", &Opcion);
+                if (strcmp(contactos[i].nombre, temp) == 0) /* Si un contacto coincide, este sera seleccionado */
+                {
+                    /* Menu de Eliminar */
+                    printf("\nEsta seguro de que desea eliminar este contacto?(S o N)\n"
+                           "\nInformacion del Contacto:");
+                    printf("\nNombre: %s", contactos[i].nombre);         /* Mostrara Nombre del contacto */
+                    printf("\nNumero: %s", contactos[i].numero);         /* Mostrara Numero del contacto */
+                    printf("\nDireccion: %s\n", contactos[i].direccion); /* Mostrara Direccion del contacto */
+                    scanf("%c", &Opcion);
+                    getchar();
 
-                if (Opcion == 's' || Opcion == 'S')
-                {
-                    /* En caso de ser S la opcion, este sera eliminado */
-                    contactos[i].eliminado = true;
-                    printf("Contacto Eliminado con Exito, volviendo al Menu Principal...\n");
-                    getchar();
-                    system("cls||clear");
-                    menu();
+                    if (Opcion == 's' || Opcion == 'S')
+                    {
+                        /* En caso de ser S la opcion, este sera eliminado */
+                        contactos[i].eliminado = true;
+                        printf("Contacto Eliminado con Exito, volviendo al Menu Principal...\n");
+                        getchar();
+                        system("cls||clear");
+                        menu();
+                    }
+                    else
+                    {
+                        /* En caso de ser N se devolvera al menu de inicio */
+                        printf("Contacto no Eliminado, volviendo al Menu Principal...\n");
+                        getchar();
+                        system("cls||clear");
+                        menu();
+                    }
                 }
-                else
-                {
-                    /* En caso de ser N se devolvera al menu de inicio */
-                    printf("Contacto no Eliminado, volviendo al Menu Principal...\n");
-                    getchar();
-                    system("cls||clear");
-                    menu();
-                }
-            }
-            else
-            {
-                /* Si no coincide ningun contacto existente y que no haya sido eliminado, se devolvera al menu de inicio */
-                printf("No existe un Contacto con ese nombre, volviendo al Menu Principal...");
-                getchar();
-                system("cls||clear");
-                menu();
             }
         }
+    } while (i < 101);
+
+    if (strcmp(contactos[i].nombre, temp) != 0)
+    {
+        /* Si no coincide ningun contacto existente y que no haya sido eliminado, se devolvera al menu de inicio */
+        printf("No existe un Contacto con ese nombre, volviendo al Menu Principal...");
+        getchar();
+        system("cls||clear");
+        menu();
     }
 }
